8a417af24ed2a5815fe23c55b7848e46531c8c84
K.Kimura <dotnsf@gmail.com>
2020-11-13 10:22:08 +0900

    no need for ejs

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 //. app.js
-
 var express = require( 'express' ),
-    ejs = require( 'ejs' ),
     app = express();
 
 var settings = require( './settings' );
@@ -11,13 +9,6 @@ app.use( '/paypay', paypay );
 
 app.use( express.Router() );
 app.use( express.static( __dirname + '/public' ) );
-
-app.set( 'views', __dirname + '/views' );
-app.set( 'view engine', 'ejs' );
-
-app.get( '/', function( req, res ){
-  res.render( 'index', { settings: settings } );
-});
 
 var port = process.env.PORT || 8080;
 app.listen( port );
